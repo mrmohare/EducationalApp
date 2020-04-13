e0607eb5e11af5dc0d31619b3dde6407c26366ab
mrmohare <63512686+mrmohare@users.noreply.github.com>
2020-04-13 13:19:38 +0530

    Add files via upload

---
+++ educationalApp/src/test/java/com/educationalApp/EducationalAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educationalApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducationalAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
